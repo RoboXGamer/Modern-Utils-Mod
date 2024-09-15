b2d8cb1141a3afc1ce58c19baec89586836f68b7
RoboXGamer <roboxgamer52@gmail.com>
2024-09-15 17:12:01 +0530

    Fix client side block placement bug when opening the block entity screen

--- src/main/java/net/roboxgamer/tutorialmod/block/custom/MechanicalCrafterBlock.java
+++ src/main/java/net/roboxgamer/tutorialmod/block/custom/MechanicalCrafterBlock.java
@@ -3,9 +3,7 @@ package net.roboxgamer.tutorialmod.block.custom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.*;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -15,7 +13,6 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.neoforged.neoforge.items.ItemStackHandler;
 import net.roboxgamer.tutorialmod.block.entity.ModBlockEntities;
 import net.roboxgamer.tutorialmod.block.entity.custom.MechanicalCrafterBlockEntity;
 import org.jetbrains.annotations.NotNull;
@@ -69,18 +66,18 @@ public class MechanicalCrafterBlock extends Block implements EntityBlock {
     super.onRemove(state, level, pos, newState, movedByPiston);
   }
   
- 
-  
   @Override
-  protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, Player player, @NotNull BlockHitResult hitResult) {
+  protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+    if (level.isClientSide) {
+      return InteractionResult.SUCCESS;
+    }
     var hand = player.getUsedItemHand();
     if (hand != InteractionHand.MAIN_HAND) return InteractionResult.PASS;
     BlockEntity be = level.getBlockEntity(pos);
     if (!(be instanceof MechanicalCrafterBlockEntity blockEntity)) return InteractionResult.PASS;
     if (!level.isClientSide() && player instanceof ServerPlayer serverPlayer) {
       serverPlayer.openMenu(blockEntity, pos);
-      return InteractionResult.SUCCESS;
     }
-    return InteractionResult.FAIL;
+    return InteractionResult.CONSUME;
   }
 }
