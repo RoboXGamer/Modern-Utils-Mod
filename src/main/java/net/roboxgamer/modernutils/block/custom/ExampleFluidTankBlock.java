93c2acc45f6daf90496c293122d610d1c08a5414
RoboXGamer <roboxgamer52@gmail.com>
2024-11-13 11:53:50 +0530

    Add loottable for fluid tank and fix to blacklist buckets having mob in them

--- src/main/java/net/roboxgamer/modernutils/block/custom/ExampleFluidTankBlock.java
+++ src/main/java/net/roboxgamer/modernutils/block/custom/ExampleFluidTankBlock.java
@@ -5,10 +5,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -44,7 +41,7 @@ public class ExampleFluidTankBlock extends Block implements EntityBlock {
     Item item = stack.getItem();
     
     // Case 1: Player is holding a filled bucket
-    if (item instanceof BucketItem bucketItem) {
+    if (item instanceof BucketItem bucketItem && !(item instanceof MobBucketItem)) {
       Fluid fluidInBucket = bucketItem.content;
       
       // If the bucket is filled with a fluid, try to insert it into the tank
