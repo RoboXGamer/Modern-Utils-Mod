531803ccc90f2f9308d66941264a99923148d663
RoboXGamer <roboxgamer52@gmail.com>
2025-03-04 14:02:02 +0530

    Refactor SideConfigButton to use ISidedMachine interface for improved flexibility

--- src/main/java/net/roboxgamer/modernutils/client/screen/SideConfigButton.java
+++ src/main/java/net/roboxgamer/modernutils/client/screen/SideConfigButton.java
@@ -9,7 +9,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.roboxgamer.modernutils.block.entity.custom.MechanicalCrafterBlockEntity;
 import net.roboxgamer.modernutils.util.Constants;
 import net.roboxgamer.modernutils.util.PackedButtonData;
 import org.jetbrains.annotations.NotNull;
@@ -18,7 +17,8 @@ import java.util.Objects;
 
 public class SideConfigButton extends ExtendedButton {
   private final Constants.Sides side;
-  private MechanicalCrafterBlockEntity blockEntity = null;
+  private Constants.ISidedMachine sidedMachine = null;
+  private BlockEntity blockEntity = null;
   private final AbstractContainerScreen<?> screen;
   
   public SideConfigButton(
@@ -38,8 +38,9 @@ public class SideConfigButton extends ExtendedButton {
         player
     );
     this.side = side;
-    if (blockEntity instanceof MechanicalCrafterBlockEntity mechanicalCrafter) {
-      this.blockEntity = mechanicalCrafter;
+    this.blockEntity = blockEntity;
+    if (blockEntity instanceof Constants.ISidedMachine machine) {
+      this.sidedMachine = machine;
     }
     this.screen = screen;
   }
@@ -47,8 +48,7 @@ public class SideConfigButton extends ExtendedButton {
   @Override
   public void renderIcon(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, ExtendedButton extendedButton) {
     // Get the current mode of the side
-    Constants.SideState sideMode = this.blockEntity.getSideManager().getSideState(side);
-    
+    Constants.SideState sideMode = this.sidedMachine.getSideManager().getSideState(side);
     // Determine the background color based on the mode
     int backgroundColor = Constants.getColorForMode(sideMode);
     int margin = 2;
@@ -69,7 +69,7 @@ public class SideConfigButton extends ExtendedButton {
     guiGraphics.pose().translate(this.getX() + offset, this.getY() + offset, 0);
     guiGraphics.pose().scale(scale, scale, 1);
     ItemStack item = ItemStack.EMPTY;
-    var pos = this.blockEntity.getBlockPos().relative(this.blockEntity.getSideManager().getRelativeDirection(side));
+    var pos = this.blockEntity.getBlockPos().relative(this.sidedMachine.getSideManager().getRelativeDirection(side));
     var state = Objects.requireNonNull(this.blockEntity.getLevel()).getBlockState(pos);
     if (state.hasBlockEntity() && this.blockEntity.getLevel().isLoaded(pos)){
       item = state.getBlock().asItem().getDefaultInstance();
@@ -84,22 +84,13 @@ public class SideConfigButton extends ExtendedButton {
   
   @Override
   public void renderTooltip(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY) {
-    if (blockEntity instanceof MechanicalCrafterBlockEntity mechanicalCrafter) {
-      Constants.SideState sideState = mechanicalCrafter.getSideManager().getSideState(side);
+      Constants.SideState sideState = sidedMachine.getSideManager().getSideState(side);
       Component msg = Component.literal(String.format("%s Side, State: %s", side, sideState));
       guiGraphics.renderTooltip(Minecraft.getInstance().font, msg, mouseX, mouseY);
     }
-  }
   
   public Constants.Sides getSide() {
-    return side;
-  }
-  
-  public Constants.SideState getSideState() {
-    if (blockEntity instanceof MechanicalCrafterBlockEntity mechanicalCrafter) {
-      return mechanicalCrafter.getSideManager().getSideState(side);
-    }
-    return Constants.SideState.NONE;
+    return this.side;
   }
   
   private static ExtendedButton.OnPressExtended sendToServerAction(
@@ -119,11 +110,9 @@ public class SideConfigButton extends ExtendedButton {
   }
   
   public void updateTooltip() {
-    if (blockEntity instanceof MechanicalCrafterBlockEntity) {
-      Constants.SideState sideState = getSideState();
+      Constants.SideState sideState = sidedMachine.getSideManager().getSideState(side);
       this.setTooltip(Tooltip.create(
           Component.literal(String.format("%s Side, State: %s", side, sideState))
       ));
-    }
   }
 }
